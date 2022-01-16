d2909eaf62da90100f3fdcd5bd6e3b5b16dc2b4a
Vivek Jaiswal <vivekjai10@gmail.com>
2022-01-16 20:46:32 +0530

    Update weather route to return weather report.

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends
 from typing import Optional
 from models.location import Location
+from services import openweather
 
 router = APIRouter()
 
 
 @router.get("/api/weather/{city}")
-def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
-    return f"{loc.city} - {loc.state} - {loc.country} - {units}"
+async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+    report = await openweather.get_weather_report(loc.city, loc.state, loc.country, units)
+    return report
